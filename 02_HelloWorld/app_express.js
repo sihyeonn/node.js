5ee1c9e5f39a163b403d786d9bf1eb8a35141920
Sihyeon Jang <uneedsihyeon@gmail.com>
2019-12-29 01:34:23 -0500

    Update app_express.js

--- 02_HelloWorld/app_express.js
+++ 02_HelloWorld/app_express.js
@@ -2,27 +2,51 @@ const express = require('express');
 const app = express();
 const host = '127.0.0.1';
 const port = 3000;
+const default_lang = 'english';
+
+const HELLO = {
+    'french': 'Bonjour',
+    'spanish': 'Hola',
+    'english': 'Hello'
+};
+
+const WORLD = {
+    'french': 'Monde',
+    'spanish': 'Mundo',
+    'english': 'World'
+};
 
 app.listen(port, () => { // = app.listen(port, host, () => {});
     console.log(`http://${host}:${port}`);
 });
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
-    console.log("I got a request");
+    let name = (req.query.name) ? req.query.name : WORLD[default_lang];
+    res.send(`<h1>${HELLO[default_lang]}, ${name}!</h1>`);
 });
 
-app.get("/french", (req, res) => {
-    res.send("Bonjour Monde")
+app.get("/api/user", (req, res) => {
+    let users = { // json
+        user: [
+            {id: 0, name: "Su", type: 'O'},
+            {id: 1, name: "Si", type: 'A'},
+            {id: 2, name: "Sa", type: 'A'},
+            {id: 3, name: "So", type: 'O'}
+        ],
+        cnt: 4
+    };
+    res.json(users);
 });
 
-app.get("/spanish", (req, res) => {
-    res.send("Hola Mundo")
+app.get("/language/:lang/:name", (req, res) => { // "/blog/:category/:id"
+    let lang = req.params.lang;
+    let name = req.params.name;
+    res.send(`${HELLO[lang]}, ${name}!`);
 });
 
-app.get("/home", (req, res) => {
-    let name = req.query.name;
-    if (name === undefined)
-        name = 'World';
-    res.send(`<h1>Hello, ${name}!</h1>`);
+app.get("/language/:lang", (req, res) => {
+    let lang = req.params.lang;
+    let name = (req.query.name) ? req.query.name : WORLD[lang];
+    res.send(`${HELLO[lang]}, ${name}!`);
 });
+
