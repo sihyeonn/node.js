205cb5426c12832ceebf67dcbfbd31db910ace18
Sihyeon Jang <uneedsihyeon@gmail.com>
2020-01-05 03:18:20 -0500

    Add comments to understand easily

--- 04_Pug/public/js/promise.js
+++ 04_Pug/public/js/promise.js
@@ -1,11 +1,14 @@
 function init() {
   var date = getDays().then(function(d){
-    console.log(d.getDate());
+    console.log(d.getDay());
   });
+  console.log('init'); // Promise:: Execute right away after L3
 }
 
+// await works only in async function
 async function async_init() {
   var date = await getDays();
+  console.log('async_init'); // async-await:: Execute after finishing L10
   console.log(date.getDate());
 }
 
@@ -18,3 +21,4 @@ function getDays() {
 
 init();
 async_init();
+console.log('here'); // Execute right away after L23
