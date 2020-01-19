0dd2b67b0f45bb74ebda8f3edd21df289bc60ab4
Sihyeon Jang <uneedsihyeon@gmail.com>
2020-01-18 21:38:52 -0500

    Change relative path to absolute path

--- 04_Board/app.js
+++ 04_Board/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
@@ -7,8 +8,9 @@ const host = '127.0.0.1';
 app.listen(port, () => { console.log(`http://${host}:${port}`); });
 
 app.set('view engine', 'pug');
-app.set('views', './views'); // find files for view engine like render
-app.use('/', express.static('./public'));
+app.set('views', aPath('./views')); // find files for view engine like render
+app.use('/', express.static(aPath('./public')));
+app.use('/uploads', express.static(aPath('./uploads'))); // without this line, clients can not access to uploads directory
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.locals.pretty = true;
@@ -23,7 +25,11 @@ app.use(methodOverride((req, res) => {
 
 
 /* Router */
-const boardRouter = require("./router/board");
-const apiRouter = require("./router/api");
+const boardRouter = require(aPath("./router/board"));
+const apiRouter = require(aPath("./router/api"));
 app.use("/board", boardRouter);
 app.use("/api", apiRouter);
+
+function aPath(cPath) {
+  return path.join(__dirname, cPath);
+}
