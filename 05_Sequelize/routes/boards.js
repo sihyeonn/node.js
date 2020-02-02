88721d610ae012f5bf9f147c665f21db7d7cfc43
Sihyeon Jang <uneedsihyeon@gmail.com>
2020-02-02 02:19:17 -0500

    Fix config file bug and add date-time

--- 05_Sequelize/routes/boards.js
+++ 05_Sequelize/routes/boards.js
@@ -1,10 +1,12 @@
 var express = require('express');
+const dateTime = require('date-time');
 var router = express.Router();
 var {Board} = require('../models');
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-  const rows = await Board.findAll();
+  const rows = await Board.findAll({ order: [["id", "desc"]], raw: true });
+  rows.forEach(row => row.createdAt = dateTime({date: row.createdAt}));
   res.render('board-list.pug', { rows });
 });
 
