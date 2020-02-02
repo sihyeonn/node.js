6f6e43dd038e9b8c57d5681db1ddbbd5c9e3a0a0
Sihyeon Jang <uneedsihyeon@gmail.com>
2020-02-02 01:35:00 -0500

    Add method-override package

--- 05_Sequelize/app.js
+++ 05_Sequelize/app.js
@@ -5,9 +5,11 @@ var cookieParser = require('cookie-parser');
 var fs = require('fs')
 var rfs = require('rotating-file-stream')
 var logger = require('morgan');
+var methodOverride = require('method-override')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var boardsRouter = require('./routes/boards');
 
 var app = express();
 var {sequelize, Sequelize} = require('./models')
@@ -37,6 +39,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// NOTE: when using req.body, you must fully parse the request body
+//       before you call methodOverride() in your middleware stack,
+//       otherwise req.body will not be populated.
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
