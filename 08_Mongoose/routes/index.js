e8b190cc74dae2d5b948256c51e813ebfd31a816
Sihyeon Jang <uneedsihyeon@gmail.com>
2020-02-16 01:44:54 -0500

    Add delete feature

--- 08_Mongoose/routes/index.js
+++ 08_Mongoose/routes/index.js
@@ -4,24 +4,40 @@ const User = require('../schemas/user');
 const { alert } = require('../modules/util');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log( User.find() );
-  res.render('index.pug');
+router.get('/', async function(req, res, next) {
+  const result = await User.find();
+  res.render('index.pug', {result});
 });
 
 router.post('/user/save', async (req, res, next) => {
   const { name, age } = req.body;
-  const oldUser = await User.find({ name })
+  const oldUser = await User.findOne({ name })
   console.log(oldUser);
-  if (oldUser.length) res.send(alert('This name is already used.', '/'));
+  if (oldUser) res.send(alert('This name is already used.', '/'));
   else {
     const user = new User({ name, age });
     // user.save().then((result) => { res.json(result); }).catch((err) => { next(err) });
     try {
       const result = await user.save();
-      res.json(result);
+      res.send(alert('Saved!', '/'));
     } catch(e) { next(e); }
   }
 })
+
+router.get('/user/delete/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const oldUser = await User.findOne({ _id: id });
+  if (oldUser) {
+    const result = await User.remove({ _id: id });
+    if (result.ok === 1) res.send(alert('Removed!', '/'));
+    else res.send(alert('Failed to remove!', '/'));
+  } else {
+    res.send(alert('There is nothing to delete!', '/'));
+  }
+})
+
+/* update
+const result = User.update({ _id: req.params.id }, { age: 99 });
+*/
 
 module.exports = router;
