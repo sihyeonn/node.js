975fb30570eab5e47321add02cd3b736fba2032d
Sihyeon Jang <uneedsihyeon@gmail.com>
2020-02-16 01:20:14 -0500

    Use mongoose and treat MongoDB

--- 08_Mongoose/routes/index.js
+++ 08_Mongoose/routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../schemas/user');
+const { alert } = require('../modules/util');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log( User.find() );
+  res.render('index.pug');
 });
+
+router.post('/user/save', async (req, res, next) => {
+  const { name, age } = req.body;
+  const oldUser = await User.find({ name })
+  console.log(oldUser);
+  if (oldUser.length) res.send(alert('This name is already used.', '/'));
+  else {
+    const user = new User({ name, age });
+    // user.save().then((result) => { res.json(result); }).catch((err) => { next(err) });
+    try {
+      const result = await user.save();
+      res.json(result);
+    } catch(e) { next(e); }
+  }
+})
 
 module.exports = router;
--- 08_Mongoose/schemas/index.js
+++ 08_Mongoose/schemas/index.js
@@ -6,4 +6,7 @@ module.exports = () => {
   connect();
   mongoose.connection.on('error', (err) => { console.log(err); });
   mongoose.connection.on('disconnected', () => { console.log('Retry to connect MongoDB'); connect(); });
+
+  require('./user');
+//  require('./comment');
 }
