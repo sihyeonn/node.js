3cfeb64a15d6678807dff650c0e4154baf402e69
Sihyeon Jang <uneedsihyeon@gmail.com>
2020-02-15 23:50:38 -0500

    Connect MongoDB with mongoose

---
+++ 08_Mongoose/schemas/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+module.exports = () => {
+  const connect = () => {
+    mongoose.connect('mongodb://localhost:15000/node', {}, (err) => { console.log(err || "Connected MongoDB"); });
+  }
+  connect();
+  mongoose.connection.on('error', (err) => { console.log(err); });
+  mongoose.connection.on('disconnected', () => { console.log('Retry to connect MongoDB'); connect(); });
+}
