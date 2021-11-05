4963acaa2de3f9125bdb154e62df2eff2709a0c9
Sihyeon Jang <uneedsihyeon@gmail.com>
2021-11-05 18:21:46 +0900

    ADD - practice of f
    Signed-off-by: Sihyeon Jang <uneedsihyeon@gmail.com>

---
+++ Python/11A-func.py
@@ -0,0 +1,15 @@
+def hello():
+    print("Hello Python!")
+
+
+def hello2(name):
+    print("Hello,", name)
+
+
+def square(x):
+    return x * x
+
+
+hello()
+hello2("Sihyeon")
+print("2 * 2 =", square(2))
