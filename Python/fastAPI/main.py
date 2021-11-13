852c82a15a62c9e5cb6d92eca5fd522190020805
Sihyeon Jang <uneedsihyeon@gmail.com>
2021-11-14 08:17:48 +0900

    ADD - Starlette example
    Signed-off-by: Sihyeon Jang <uneedsihyeon@gmail.com>

--- Python/fastAPI/main.py
+++ Python/fastAPI/main.py
@@ -2,9 +2,18 @@ from typing import Optional
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from starlette.applications import Starlette
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
+
+templates = Jinja2Templates(directory='templates')
 
 
 app = FastAPI()
+
+
+app.mount('/static', StaticFiles(directory='statics'), name='static')
 
 
 class Item(BaseModel):
@@ -17,11 +26,32 @@ class Item(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id, "is_offer": item.is_offer}
+
+
+@app.route("/error")
+async def error(request):
+    raise RuntimeError("Oh no")
+
+
+@app.exception_handler(404)
+async def not_found(request, exc):
+    template = "404.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context, status_code=404)
+
+
+@app.exception_handler(500)
+async def server_error(request, exc):
+    template = "500.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context, status_code=500)
 
