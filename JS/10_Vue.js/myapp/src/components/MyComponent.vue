baff0bac007f954668c04712e639976f877a3755
Sihyeon Jang <uneedsihyeon@gmail.com>
2022-03-01 11:36:59 +0900

    feat: vue myapp and MyComponent
    Signed-off-by: Sihyeon Jang <uneedsihyeon@gmail.com>

---
+++ JS/10_Vue.js/myapp/src/components/MyComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  props: {
+    msg: {
+      type: String,
+      optional: true,
+      default: 'MyComponent'
+    }
+  },
+  data() {
+    return {
+      message: this.msg ?? 'This is a MyComponent'
+    }
+  }
+}
+</script>
